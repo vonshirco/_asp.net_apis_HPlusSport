6e0b2fbbd1f0e18836252579084c919dcf0698af
vonshirco <vonshirco.vs@gmail.com>
2023-05-11 21:17:39 +0300

    Add project files.

---
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -0,0 +1,123 @@
+﻿using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")] //Route Attribute
+    [ApiController]
+    public class ProductsController : ControllerBase //Inheritated from :
+    {
+        //Constructor of the Product Controller and set the database context It will be injected)
+        private readonly ShopContext _Context;
+
+        public ProductsController(ShopContext context)
+        {
+            _Context = context;
+
+            _Context.Database.EnsureCreated(); //Because we are seeding the database
+        }
+
+        [HttpGet] //HttpGet Attribute
+        public  async Task<ActionResult> GetAllProducts()
+        {
+            return Ok(await _Context.Products.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetProduct(int id) {
+            var product =await _Context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            /*
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }*/
+            _Context.Products.Add(product); // _context - Database context
+            await _Context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                "GetProduct",
+                new { id = product.Id },
+                product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutProduct(int id, Product product)
+        {
+            if(id != product.Id)
+            {
+                return BadRequest();
+            }
+            //Saving the data
+            _Context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _Context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) //Maybe some products have already being modified by other API calls
+            { 
+                if(!_Context.Products.Any(p => p.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw; //Throw the expection and then the Http requrest will just be serialized by Json
+                }
+
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _Context.Products.FindAsync(id);
+            if (product == null) 
+            {
+                return NotFound();
+            }
+
+            _Context.Products.Remove(product);
+            await _Context.SaveChangesAsync();
+
+            return product;
+        }
+
+        [HttpPost("{id}")]
+        [Route("Delete")]
+        public async Task<ActionResult> DeleteMultiple([FromQuery]int [] ids)
+        {
+            var products = new List<Product>();
+            foreach (var id in ids)
+            {
+                var product = await _Context.Products.FindAsync(id);
+
+                if(product == null)
+                {
+                    return NotFound();
+                }
+
+                products.Add(product);
+            }
+
+            _Context.Products.RemoveRange(products);
+            await _Context.SaveChangesAsync();
+
+            return Ok(products);
+        }
+    }
+}
---
+++ HPlusSport.API/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace HPlusSport.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required] //For Sku
+        public string Sku { get; set; } = string.Empty;
+        [Required] //For Name
+        public string Name { get; set; } = string.Empty;
+        [Required] //For Description
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; } 
+        public bool IsAvailable { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        [JsonIgnore]
+        public virtual Category? Category { get; set; }
+
+    }
+}
